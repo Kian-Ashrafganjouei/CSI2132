a880819e775fea2a853d352983ea830d58e0269d
Kian <kashr085@uottawa.ca>
2024-03-28 22:08:46 -0400

    Added stuff for customer, fixed links

--- frontend/customer/customer.js
+++ frontend/customer/customer.js
@@ -62,18 +62,32 @@ document.addEventListener('DOMContentLoaded', () => {
                 updateBtn.forEach(button => {
                     button.addEventListener('click', function() {
                         const row = button.closest('tr');
-                        const customerId = row.querySelector('td:nth-child(1)').innerText;
-                        console.log('Updating customer with ID:', customerId);
+                        const customerName = row.querySelector('td:nth-child(1)').innerText;
+                        const emailAddress = row.querySelector('td:nth-child(2)').innerText;
+                        const phoneNumber = row.querySelector('td:nth-child(3)').innerText;
+                        
+                        document.getElementById('customerName').value = customerName;
+                        document.getElementById('emailAddress').value = emailAddress;
+                        document.getElementById('phoneNumber').value = phoneNumber;
 
-                        // Retrieve customer information
-                        const customerName = row.querySelector('td:nth-child(2)').innerText;
-                        const emailAddress = row.querySelector('td:nth-child(3)').innerText;
-                        const phoneNumber = row.querySelector('td:nth-child(4)').innerText;
-                        const cardNumber = row.querySelector('td:nth-child(5)').innerText;
-                        const idType = row.querySelector('td:nth-child(6)').innerText;
-                        const dateOfRegistration = row.querySelector('td:nth-child(7)').innerText;
-                        const addressID = row.querySelector('td:nth-child(8)').innerText;
 
+                        const cardNumber = document.getElementById('cardNumber').value;
+                        const idType = document.getElementById('idType').value;
+                        const dateOfRegistration = document.getElementById('dateOfRegistration').value;
+                        const streetName = document.getElementById('streetName').value;
+                        const streetNumber = document.getElementById('streetNumber').value;
+                        const postalCode = document.getElementById('postalCode').value;
+                        const unitNumber = document.getElementById('unitNumber').value;
+                        const cityName = document.getElementById('cityName').value;
+                        const countryName = document.getElementById('countryName').value;
+                        // Check if any of the fields are empty
+                        if (!customerName || !emailAddress || !phoneNumber || !cardNumber || !idType || !dateOfRegistration || !dateOfRegistration || !streetNumber  || !postalCode || !cityName || !countryName) {
+                            console.error('Please fill out all fields before updating.');
+                            return;
+                        }
+                    
+                        console.log('ADDING CUSTOMER:', customerName, emailAddress, phoneNumber);
+                    
                         // Create an object with customer data
                         const customerData = {
                             customerName,
@@ -82,7 +96,14 @@ document.addEventListener('DOMContentLoaded', () => {
                             cardNumber,
                             idType,
                             dateOfRegistration,
-                            addressID
+                            address: {
+                                streetName,
+                                streetNumber,
+                                postalCode,
+                                unitNumber,
+                                cityName,
+                                countryName
+                            }
                         };
                         fetch(`/customers`, {
                             method: 'PUT',
@@ -109,44 +130,57 @@ document.addEventListener('DOMContentLoaded', () => {
     // Fetch customers on page load
     fetchCustomers();
 
-    // Add event listener to customer form for submitting new customer data
-    customerForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const customerName = document.getElementById('customerName').value;
-        const emailAddress = document.getElementById('emailAddress').value;
-        const phoneNumber = document.getElementById('phoneNumber').value;
-        const cardNumber = document.getElementById('cardNumber').value;
-        const idType = document.getElementById('idType').value;
-        const dateOfRegistration = document.getElementById('dateOfRegistration').value;
-        const addressID = document.getElementById('addressID').value;
-        console.log('ADDING CUSTOMER:', customerName, emailAddress, phoneNumber);
+// Add event listener to customer form for submitting new customer data
+customerForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const customerName = document.getElementById('customerName').value;
+    const emailAddress = document.getElementById('emailAddress').value;
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    const cardNumber = document.getElementById('cardNumber').value;
+    const idType = document.getElementById('idType').value;
+    const dateOfRegistration = document.getElementById('dateOfRegistration').value;
+    const streetName = document.getElementById('streetName').value;
+    const streetNumber = document.getElementById('streetNumber').value;
+    const postalCode = document.getElementById('postalCode').value;
+    const unitNumber = document.getElementById('unitNumber').value;
+    const cityName = document.getElementById('cityName').value;
+    const countryName = document.getElementById('countryName').value;
+    console.log('ADDING CUSTOMER:', customerName, emailAddress, phoneNumber);
 
-        // Create an object with customer data
-        const customerData = {
-            customerName,
-            emailAddress,
-            phoneNumber,
-            cardNumber,
-            idType,
-            dateOfRegistration,
-            addressID
-        };
+    // Create an object with customer data
+    const customerData = {
+        customerName,
+        emailAddress,
+        phoneNumber,
+        cardNumber,
+        idType,
+        dateOfRegistration,
+        address: {
+            streetName,
+            streetNumber,
+            postalCode,
+            unitNumber,
+            cityName,
+            countryName
+        }
+    };
 
-        fetch('/customers', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(customerData)
-        })
-        .then(response => {
-            if (response.ok) {
-                fetchCustomers(); // Fetch and display updated customer list
-                customerForm.reset(); // Clear form fields
-            } else {
-                throw new Error('Failed to add customer');
-            }
-        })
-        .catch(error => console.error('Error adding customer:', error));
-    });
+    fetch('/customers', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(customerData)
+    })
+    .then(response => {
+        if (response.ok) {
+            fetchCustomers(); // Fetch and display updated customer list
+            customerForm.reset(); // Clear form fields
+        } else {
+            throw new Error('Failed to add customer');
+        }
+    })
+    .catch(error => console.error('Error adding customer:', error));
+});
+
 });
